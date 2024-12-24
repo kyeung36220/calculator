1fb74de70fe3bcf088c9d136107b465de3dc82d2
kyeung36220 <135488116+kyeung36220@users.noreply.github.com>
2024-12-23 23:18:50 -0500

    Made code more readable
    Added comments explaining what code does and added a function called reset which organizes some of the messy variables into more readable format with if statements

--- scripts.js
+++ scripts.js
@@ -1,3 +1,39 @@
+// Initialization of all buttons into variables
+const screenText = document.querySelector(".screen .text")
+const numbers = document.querySelectorAll(".number")
+const operators = document.querySelectorAll(".operator")
+const clear = document.querySelector(".clear")
+const sign = document.querySelector(".sign")
+const percent = document.querySelector(".percent")
+const dot = document.querySelector(".dot")
+const equalSign = document.querySelector(".equal")
+
+// Initialization of all necessary variables
+let blankSlate = true
+let currentNumber = ""
+let alteringNumber = ""
+let currentOperator = undefined
+let secondNumber = false
+let afterEqual = false
+let changedToPercent = false
+let changedToFloat = false
+
+// Initialization of all button click's functions
+numbers.forEach(number => {
+    number.addEventListener("click", numberClicked)
+})
+
+operators.forEach(operator => {
+    operator.addEventListener("click", operatorClicked)
+})
+
+equalSign.addEventListener("click", equal)
+clear.addEventListener("click", clearText)
+sign.addEventListener("click", signChange)
+percent.addEventListener("click", changeToPercent)
+dot.addEventListener("click", changeToFloat)
+
+// Math functions
 function add() {
     return currentNumber + alteringNumber
 }
@@ -14,23 +50,25 @@ function divide() {
     return currentNumber / alteringNumber
 }
 
+// When AC is pressed
 function clearText() {
     screenText.textContent = ""
     currentNumber = ""
     alteringNumber = ""
-    blankSlate = true
     currentOperator = "nan"
-    secondNumber = false
-    changedToPercent = false
-    changedToFloat = false
+    reset("clearText")
 }
 
+// When any digit is pressed
 function numberClicked(e) {
     let number = e.target.value
+
+    // prevents more numbers after percent sign
     if (changedToPercent == true) {
         return
     }
 
+    // resets entire calc if number is pressed after equal sign
     else if (afterEqual == true) {
         clearText()
         currentNumber += number
@@ -38,17 +76,19 @@ function numberClicked(e) {
         afterEqual = false
     }
 
+    // sees if number is the first number in a calculation
     else if (blankSlate == true) {
         currentNumber += number
         screenText.textContent = currentNumber
     }
 
+    // sees if number is second number
     else if (secondNumber == true) {
-        blankSlate = false
         alteringNumber += number
         screenText.textContent = alteringNumber
     }
 
+    // if first number
     else {
         alteringNumber += number
         screenText.textContent = alteringNumber
@@ -58,11 +98,27 @@ function numberClicked(e) {
 function operatorClicked(e) {
     alteringNumber = ""
     currentOperator = e.target.value
-    secondNumber = true
+    reset("operatorClicked")
+}
+
+function reset(functionName) {
     changedToPercent = false
     changedToFloat = false
-    afterEqual = false
-    blankSlate = false
+    if (functionName === "clearText") {
+        blankSlate = true
+        afterEqual = false
+        secondNumber = false
+    }
+    else if (functionName === "operatorClicked") {
+        blankSlate = false
+        afterEqual = false
+        secondNumber = true
+    }
+    else if (functionName === "equal") {
+        blankSlate = false
+        afterEqual = true
+        secondNumber = false
+    }
 }
 
 function equal() {
@@ -94,11 +150,7 @@ function equal() {
 
     screenText.textContent = total
     currentNumber = total
-    secondNumber = false
-    afterEqual = true
-    changedToPercent = false
-    changedToFloat = false
-    blankSlate = false
+    reset("equal")
 }
 
 function signChange() {
@@ -143,39 +195,3 @@ function changeToFloat() {
     }
     changedToFloat = true
 }
-
-const screenText = document.querySelector(".screen .text")
-const numbers = document.querySelectorAll(".number")
-const operators = document.querySelectorAll(".operator")
-const clear = document.querySelector(".clear")
-const sign = document.querySelector(".sign")
-const percent = document.querySelector(".percent")
-const dot = document.querySelector(".dot")
-const equalSign = document.querySelector(".equal")
-
-let blankSlate = true
-let currentNumber = ""
-let alteringNumber = ""
-let currentOperator = undefined
-let secondNumber = false
-let afterEqual = false
-let changedToPercent = false
-let changedToFloat = false
-
-numbers.forEach(number => {
-    number.addEventListener("click", numberClicked)
-})
-
-operators.forEach(operator => {
-    operator.addEventListener("click", operatorClicked)
-})
-
-equalSign.addEventListener("click", equal)
-
-clear.addEventListener("click", clearText)
-
-sign.addEventListener("click", signChange)
-
-percent.addEventListener("click", changeToPercent)
-
-dot.addEventListener("click", changeToFloat)
